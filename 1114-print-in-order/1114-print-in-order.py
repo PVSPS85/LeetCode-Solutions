19dc7530b1baa4c160838353cdb48759a98576ed
Pratap Veera Sai Pranav <pvsaipranav2007@gmail.com>
2026-08-24 17:10:52 +0530

    Time: 68 ms (26.78%), Space: 19.7 MB (95.73%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.py
@@ -0,0 +1,20 @@
+import threading
+
+class Foo:
+    def __init__(self):
+        self.first_done = threading.Event()
+        self.second_done = threading.Event()
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        printFirst()
+        self.first_done.set()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.first_done.wait()
+        printSecond()
+        self.second_done.set()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        # Fix here: Change from first_done to second_done
+        self.second_done.wait() 
+        printThird()
